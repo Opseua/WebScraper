bc210435b7cd172e0fa1270232fc4ffe60aa4dda
Orlando <opseua@gmail.com>
2024-04-16 10:57:28 -0300

    'getPath' e 'devChildren'

--- src/resources/@export.js
+++ src/resources/@export.js
@@ -1,6 +1,20 @@
-// FUNCTIONS
+// DEFINIR O 'devChildren' → [CHROME] EMAIL DO USUÁRIO | [NODEJS] PRIMEIRO ARQUIVO A SER EXECUTADO (NA MAIORIA DOS CASOS 'server')
+let devC = new Error().stack.split('\n'); devC = devC[devC.length - 1]; let devChildren = devC.includes('.js:') ? devC.match(/\/([^/]+)\.[^/]+$/)[1] : false
+if (typeof window !== 'undefined') { devChildren = await new Promise((resolve) => { chrome.identity.getProfileUserInfo(function (u) { resolve(u.email) }) }) }
+
+// @functions
 await import('../../../Chrome_Extension/src/resources/@functions.js');
-await getPath({ 'e': new Error(), 'mode': 1, 'keep': true })
+
+// DEFINIR → LETTER | ROOT | FUNCTION | PROJECT | FILE | LINE
+let retGetPath = await getPath({ 'e': new Error(), 'devChildren': devChildren })
+
+// console.log(eng, '-', engName, '-', cng, '-', letter); console.log("#################")
+// console.log('securityPass:', globalWindow.securityPass)
+// console.log('portWeb:', globalWindow.portWeb, '|', 'serverWeb:', globalWindow.serverWeb)
+// console.log('portLoc:', globalWindow.portLoc, '|', 'serverLoc:', globalWindow.serverLoc)
+// console.log('devMaster:', globalWindow.devMaster, '|', 'devSlave:', globalWindow.devSlave, '|', 'devChildren:', globalWindow.devChildren)
+// console.log('devSend:', globalWindow.devSend)
+// console.log('devGet:', globalWindow.devGet)
 
 // FUNÇÕES DESSE PROJETO
 await import('./apiCnpj.js')
