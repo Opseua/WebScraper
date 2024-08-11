2e034c4eb9f1470225f2e7b1477ae4b4a24640fe
Orlando <opseua@gmail.com>
2024-08-11 05:12:01 -0300

    Correção do IF

--- src/z_Outros_serverJucesp/2_SCRIPT.bat
+++ src/z_Outros_serverJucesp/2_SCRIPT.bat
@@ -25,7 +25,7 @@ rem #### ↑↑↑↑↑↑↑↑↑ ###########################################
 
 rem CHECAR SE ESTA RODANDO
 rem tasklist /fi "ImageName eq node!project!_!outrosAdd!.exe" /fo csv 2>NUL | find /I "node!project!_!outrosAdd!.exe">NUL
-rem if "%ERRORLEVEL%"=="0"  ( set "ret=TRUE" ) else ( set "ret=FALSE" )
+rem if "%ERRORLEVEL%"=="0" ( set "ret=TRUE" ) else ( set "ret=FALSE" )
 
 rem ESTA RODANDO [NAO]
 rem if "!ret!"=="FALSE" ( if "!arg1!"=="!arg1:OFF=!" ( ) )
